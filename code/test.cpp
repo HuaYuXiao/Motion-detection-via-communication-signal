7b24212b94a99c4a669b5007f7a11f1812589fe0
Yuxiao Hua <117464811+HuaYuXiao@users.noreply.github.com>
2023-04-19 15:45:44 +0800

    Delete test.cpp

--- code/test.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>  
-int main(){
-    for(int i = 0 ; i < 100; i++){
-        static int a = 1;
-        std::cout << a << " || ";
-        a = !a;
-        std::cout << a << std::endl;
-    }
-}
